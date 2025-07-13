0c83d7da2a5d6f9f56cb90605947dcb77b478c1b
ghanizadev <jf.melo6@gmail.com>
2025-07-13 22:12:53 +0300

    Adicionar suporte para requisicoes OPTIONS na rota de status

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -1,5 +1,16 @@
 import { database } from "@/database";
 
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
+
 export async function GET() {
   const status = await database.get("status");
   return new Response(status ?? "IDLE", {
